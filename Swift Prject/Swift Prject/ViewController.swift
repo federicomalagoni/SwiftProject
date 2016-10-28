79a36b34a6a06c0cf195fc45f950eaacc011349f
whatweneed <malagoni.federico@gmail.com>
2016-10-28 16:00:43 +0200

    added bg color

---
+++ Swift Prject/Swift Prject/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Swift Prject
+//
+//  Created by Federico Malagoni on 28/10/2016.
+//  Copyright © 2016 Federico Malagoni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.view.backgroundColor = UIColor.lightGrayColor()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
